c631849b7c16e1f24b1f6a425bf6dc6b1c559785
mitubakuro <nakano1113mituba@gmail.com>
2020-08-05 11:17:03 +0900

    room_usersテーブルの作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
   def new
-    @romms = Room.new
+    @rooms = Room.new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true
 
-
+  has_many :room_users
+  has_many :rooms, through: :room_users
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
